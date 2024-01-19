2b60faf95ef210f89425770e3d197c6cc8237205
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2024-01-19 09:15:39 -0600

    initial setup

---
+++ app.js
@@ -0,0 +1,34 @@
+// app.js
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var cors = require('cors');
+
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('trust proxy', 1);
+app.enable('trust proxy');
+
+app.use(
+    cors({
+      origin: [process.env.REACT_APP_URI] 
+    })
+  );
+
+// app.use(
+//     cors()
+//   );
+
+app.use('/users', usersRouter);
+
+module.exports = app;
